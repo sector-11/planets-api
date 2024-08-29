a43b904b836cd03d3acbaab7d980a65a42cfa8fc
jmc <120180071+sector-11@users.noreply.github.com>
2024-08-29 15:49:04 +0100

    initial commit

---
+++ src/main/java/com/northcoders/planetsapi/PlanetsapiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.planetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsapiApplication.class, args);
+	}
+
+}
